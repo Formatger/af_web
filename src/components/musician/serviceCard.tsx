5d57adb6971f95d1afa0a42f9168ec8d5be306f6
Albert Formatger <albert_furma@hotmail.com>
2024-11-07 14:06:01 +0100

    22

--- src/components/musician/serviceCard.tsx
+++ src/components/musician/serviceCard.tsx
@@ -1,39 +1,56 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 import Image from 'next/image';
+import { Swiper as SwiperClass } from 'swiper/types';
 
 interface ServiceCardProps {
     image: string;
-    title: string,
+    title: string;
     list: string[];
+    swiperRef: React.RefObject<SwiperClass | null>;
 }
 
-const ServiceCard: React.FC<ServiceCardProps> = ({ image, 
-    title, list
-}) => {
+const ServiceCard: React.FC<ServiceCardProps> = ({ image, title, list, swiperRef }) => {
+
+    const [getInfo, setGetInfo] = useState<boolean>(false)
 
     return (
-        <div className="card">
-            <div className="card-image">
-                <Image
-                    src={image}
-                    alt='Musical Event'
-                    sizes="(max-width: 600px) 100vw, (max-width: 1200px) 100vw, 100vw"
-                    fill
-                    style={{
-                        objectPosition: 'right top'
-                    }}
-                    priority
-                />
+        <div className='card-distribution'>
+            <div
+                className="card"
+                onMouseEnter={() => {
+                    swiperRef.current?.autoplay?.stop();
+                    setGetInfo(true)
+                }}
+                onMouseLeave={() => {
+                    swiperRef.current?.autoplay?.start();
+                    setGetInfo(false)
+                }}
+            >
+                <div className="card-image">
+                    <Image
+                        src={image}
+                        alt="Musical Event"
+                        sizes="(max-width: 600px) 100vw, (max-width: 1200px) 100vw, 100vw"
+                        fill
+                        style={{
+                            objectPosition: 'right top',
+                        }}
+                        priority
+                    />
+                </div>
+                <div className="card-text">
+                    <h2>{title}</h2>
+                </div>
+
             </div>
-            <div className="card-text">
-                <h2>{title}</h2>
-                {/* <ul className="card-list">
+            {getInfo &&
+                <ul className="card-list">
                     {list.map((item, index) => (
                         <li key={index}>{item}</li>
                     ))}
-                </ul> */}
-            </div> 
-        </div >
+                </ul>
+            }
+        </div>
     );
 };
 
--- src/components/musician/servicePage.tsx
+++ src/components/musician/servicePage.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ServiceCard from './serviceCard';
+import { Swiper as SwiperClass } from 'swiper/types'; // Importa el tipo de Swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Scrollbar, EffectCube, EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Scrollbar, EffectCube, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-cube';
@@ -11,45 +12,37 @@ const Service: React.FC = () => {
         {
             image: '/musician/live_pic.jpg',
             title: 'Musical Events',
-            list: ['Concerts', 'Substitutions / Backup', 'Event Promoter', 'Backline Rental', 'Production and Road Management']
+            list: ['Concerts', 'Substitutions / Backup', 'Event Promoter', 'Backline Rental', 
+                'Production and Road Management']
         },
         {
             image: '/musician/teacher_pic.jpg',
             title: 'Guitar Lessons',
-            list: ['Online / In-person Modality', 'All Styles: Blues, Rock, Heavy, Funk, Jazz, Pop', 'Music Theory, Harmony, and Sight-reading', 'Composition, Recording, and Music Editing']
+            list: ['Online / In-person Modality', 'All Styles: Blues, Rock, Heavy, Funk, Jazz, Pop', 
+                'Music Theory, Harmony, and Sight-reading', 'Composition, Recording, and Music Editing']
         },
         {
             image: '/musician/studio_pic.jpeg',
             title: 'Session Musician',
-            list: ['Online / In-person Modality', 'Guitar Recording', 'Arrangement Composition for Songs', 'Musical Composition', 'Music Production']
+            list: ['Online / In-person Modality', 'Guitar Recording', 'Arrangement Composition for Songs', 
+                'Musical Composition', 'Music Production']
         }
     ];
 
-
-    // const services = [
-    //     {
-    //         image: '/musician/live_pic.jpg',
-
-    //     },
-    //     {
-    //         image: '/musician/teacher_pic.jpg',
-
-    //     },
-    //     {
-    //         image: '/musician/studio_pic.jpeg',
-
-    //     }
-    // ];
+    const swiperRef = useRef<SwiperClass | null>(null);
 
     return (
-        <div className='service-container'>
+        <div className="service-container">
             <h1 className="h1-white">SERVICES</h1>
-            <div className='container-card'>
+            <div className="container-card">
                 <Swiper
+                    onSwiper={(swiperInstance) => (swiperRef.current = swiperInstance)} // Asignar la instancia de Swiper
                     modules={[Scrollbar, Pagination, Navigation, Autoplay, EffectCube]}
                     loop={true}
                     centeredSlides={true}
-                    autoplay={{ delay: 1000 }}
+                    autoplay={{
+                        delay: 1000,
+                    }}
                     speed={5000}
                     effect="cube"
                     cubeEffect={{
@@ -63,17 +56,17 @@ const Service: React.FC = () => {
                                 image={service.image}
                                 title={service.title}
                                 list={service.list}
+                                swiperRef={swiperRef} // Pasar swiperRef directamente
                             />
                         </SwiperSlide>
                     ))}
                 </Swiper>
-            </div >
-        </div >
+            </div>
+        </div>
     );
 };
 
 export default Service;
-
 
 
 // const Service: React.FC = () => {
